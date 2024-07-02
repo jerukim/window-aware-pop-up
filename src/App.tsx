10c1bfe90c4461478967c4b6447138d7d5e266c1
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-02 12:40:06 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="w-[200vw] h-[200vh] grid place-items-center">
+      <button className="bg-green-500 p-4">hello world</button>
+    </div>
+  )
+}
