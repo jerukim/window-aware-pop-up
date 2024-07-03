73a5407e7b010e94b03e85fe404e893fc4419b6a
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-03 19:59:24 -0400

    playground

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import { Popup } from './components/PopUp'
 
 export default function App() {
   return (
-    <div className="w-[200vw] h-[200vh] grid place-items-center">
-      <Popup />
+    <div className="w-[200vw] h-[200vh] grid grid-cols-2 place-items-center">
+      <Popup offset={28} />
+      <Popup offset={0} />
+      <Popup offset={16} />
+      <Popup offset={16} />
+      <Popup offset={16} />
+      <Popup offset={16} />
     </div>
   )
 }
